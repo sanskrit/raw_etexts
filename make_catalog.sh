7df09616406bd9c0f6c8e1e4fd7c8b74350891b9
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-08-06 11:18:07 +0530

    CU

--- make_catalog.sh
+++ make_catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -regextype posix-extended -name "*" -not -regex ".*/(adyatithi|\.git|static)/.*" -not -regex ".*/[\d.-_]+\.(txt|md)?" -printf "%f\n" > catalog_ls.local
+find . -regextype posix-extended -name "*" -not -regex ".*/(adyatithi|\.git|static|dhavala-sangrahaH/.+/md|db_toml_md__sa__padya)/.*" -not -regex ".*/[0-9०-९.,\-_]+(txt|md|html|htm|json|toml)?" -not -regex ".*/(adhy.{,2}ya|अध्याय|_index|part).*" -printf "%f\n" > catalog_ls.local
 
 # find . -regextype posix-extended -name "*" -not -regex ".*/(adyatithi|\.git)/.*  -printf "%f\n" > catalog_ls.local
